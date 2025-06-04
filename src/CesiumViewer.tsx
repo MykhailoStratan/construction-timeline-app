bdf371e1f714e72c1454fd54d1cce6c81ba3a446
Mykhailo Stratan <36572099+MykhailoStratan@users.noreply.github.com>
2025-06-04 11:08:05 -0700

    Add building footprint drawing on building selection

--- src/CesiumViewer.tsx
+++ src/CesiumViewer.tsx
@@ -4,8 +4,21 @@ import {
   Ion,
   createWorldTerrainAsync,
   createOsmBuildingsAsync,
+  ScreenSpaceEventHandler,
+  ScreenSpaceEventType,
+  Color,
+  Cartesian3,
+  Cartographic,
+  HeightReference,
+  Entity,
+  Cesium3DTileFeature,
+  BoundingSphere,
 } from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+interface FeatureWithBoundingSphere extends Cesium3DTileFeature {
+  boundingSphere: BoundingSphere
+}
 
 const ionToken = import.meta.env.VITE_CESIUM_ION_ACCESS_TOKEN
 if (ionToken) {
@@ -21,6 +34,8 @@ const CesiumViewer = () => {
     }
 
     let viewer: Viewer | undefined
+    let handler: ScreenSpaceEventHandler | undefined
+    let footprintEntity: Entity | undefined
 
     const initialize = async () => {
       const terrainProvider = await createWorldTerrainAsync()
@@ -33,6 +48,47 @@ const CesiumViewer = () => {
         console.error('Error loading OSM Buildings', error)
       }
 
+      handler = new ScreenSpaceEventHandler(viewer.scene.canvas)
+      handler.setInputAction((click: ScreenSpaceEventHandler.PositionedEvent) => {
+        if (!viewer) {
+          return
+        }
+
+        const picked = viewer.scene.pick(click.position)
+        if (!picked || !(picked instanceof Cesium3DTileFeature)) {
+          return
+        }
+
+        const feature = picked as FeatureWithBoundingSphere
+        const bs = feature.boundingSphere
+        if (!bs) {
+          return
+        }
+
+        const centerCart = Cartographic.fromCartesian(bs.center)
+        const metersPerDegree = 111319.9
+        const delta = (bs.radius ?? 1) / metersPerDegree
+
+        const positions = [
+          Cartesian3.fromRadians(centerCart.longitude - delta, centerCart.latitude - delta, 0),
+          Cartesian3.fromRadians(centerCart.longitude + delta, centerCart.latitude - delta, 0),
+          Cartesian3.fromRadians(centerCart.longitude + delta, centerCart.latitude + delta, 0),
+          Cartesian3.fromRadians(centerCart.longitude - delta, centerCart.latitude + delta, 0),
+        ]
+
+        if (footprintEntity) {
+          viewer.entities.remove(footprintEntity)
+        }
+
+        footprintEntity = viewer.entities.add({
+          polygon: {
+            hierarchy: positions,
+            material: Color.YELLOW.withAlpha(0.5),
+            heightReference: HeightReference.CLAMP_TO_GROUND,
+          },
+        })
+      }, ScreenSpaceEventType.LEFT_CLICK)
+
       viewer.camera.flyTo({
         destination: Cesium.Cartesian3.fromDegrees(-123.102943, 49.271094, 4000),
       })
@@ -41,6 +97,7 @@ const CesiumViewer = () => {
     initialize()
 
     return () => {
+      handler?.destroy()
       viewer?.destroy()
     }
   }, [])
