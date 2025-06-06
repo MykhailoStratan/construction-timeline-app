d02c1e5181a30106f06fc564df4643db3f7de6f5
Mykhailo Stratan <36572099+MykhailoStratan@users.noreply.github.com>
2025-06-05 23:58:34 -0700

    Refactor ToolsPanel styles

--- src/ToolsPanel.tsx
+++ src/ToolsPanel.tsx
@@ -2,6 +2,7 @@ import { Viewer } from 'cesium'
 import LineDrawer from './LineDrawer'
 import Area from './Area'
 import { DrawingProvider } from './hooks/DrawingContext'
+import styles from './ToolsPanel.module.css'
 
 interface ToolsPanelProps {
   viewer: Viewer | null
@@ -10,20 +11,7 @@ interface ToolsPanelProps {
 const ToolsPanel = ({ viewer }: ToolsPanelProps) => {
   return (
     <DrawingProvider viewer={viewer}>
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          width: '60px',
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '8px',
-          padding: '8px',
-          backgroundColor: 'rgba(0,0,0,0.3)',
-        }}
-      >
+      <div className={styles.panel}>
         <LineDrawer viewer={viewer} />
         <Area viewer={viewer} />
       </div>
